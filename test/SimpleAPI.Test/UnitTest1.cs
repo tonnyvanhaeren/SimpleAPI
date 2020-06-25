ec579b1ef00c9a030bbd171b8c493180183bb9e6
Vanhaeren <antonius.vanhaeren@telenet.be>
2020-06-25 15:49:11 +0200

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetReturnsWeatherForecast()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Tonny Vanhaeren", returnValue.Value);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
